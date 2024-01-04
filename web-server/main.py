bb2e69ef26dcd72817aa0ef49303b539639c132f
Harold Torres Gallo <harold2tg@gmail.com>
2024-01-04 12:34:58 -0500

    Agregando fastApi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,32 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return ['Cebolla', 'Tomate', 'Aguacate']
+
+@app.get('/name')
+def get_name():
+    return{
+        'name': 'harold'
+    }
+    
+@app.get("/html", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>mi web</title>
+        </head>
+        <body>
+            <h1>Harold Torres Gallo!</h1>
+        </body>
+    </html>
+    """    
 
 def run():
     store.get_categories()
